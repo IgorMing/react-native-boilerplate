96df37b0e9d75abe5334d665b0652dd0366a8c51
Igor Mesquita <igor.mesquita@s2it.com.br>
2018-02-24 21:28:51 -0300

    Improved configuration files.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,22 @@
 module.exports = {
-    "extends": "airbnb"
+  "extends": "airbnb",
+  "parser": "babel-eslint",
+  "globals": {
+    "alert": true,
+    "expect": true,
+    "it": true,
+    "navigator": true,
+  },
+  "rules": {
+    "class-methods-use-this": "off",
+    "import/extensions": ["error", "never"],
+    "import/no-named-as-default": "off",
+    "max-len": ["error", { code: 150 }],
+    "no-alert": "warn",
+    "no-multiple-empty-lines": ["error", { max: 1 }],
+    "no-underscore-dangle": "off",
+    "react/forbid-prop-types": "off",
+    "react/jsx-filename-extension": "off",
+    "react/prefer-stateless-function": "warn",
+  }
 };
